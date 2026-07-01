a774b5deb600a2c491f5eadecd8185fb14258d23
Pedro-r3d <pedromourar3d@gmail.com>
2026-07-01 19:41:18 -0300

    Add files via upload

---
+++ ParOuImpar.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace ParOuImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva um número para saber se é par ou impar");
+            while (true)
+            {
+                string sair = Console.ReadLine();
+                if (sair.Trim().ToLower() == "sair")
+                {
+                    break;
+                }
+                if (!double.TryParse(sair, out double numero))
+                {
+                    Console.WriteLine("Não é um número");
+                    continue;
+                }             
+                ParImpar(numero);
+            }
+        }
+
+
+        public static void ParImpar(double numero)
+        {
+            
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+        }
+    }
+}
